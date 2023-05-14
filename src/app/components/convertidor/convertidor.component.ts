aa269ac82a57d76f7bc6de3bb190f1a80e93b454
Jesus Anaya <jesus.anaya.developer@gmail.com>
2023-05-14 13:53:40 -0600

    creacion de componentes

---
+++ src/app/components/convertidor/convertidor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convertidor',
+  templateUrl: './convertidor.component.html',
+  styleUrls: ['./convertidor.component.scss']
+})
+export class ConvertidorComponent {
+
+}
