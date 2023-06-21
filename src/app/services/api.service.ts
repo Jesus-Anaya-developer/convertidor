ef3af509604520a435bcc8c1365d388c5558f6f5
Jesus Anaya <jesus.anaya.developer@gmail.com>
2023-06-20 22:23:21 -0600

    se agrega la opcion de seleccionar monedas

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,19 +8,6 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  public getSimbolo() {
 
-    const myHeaders = new Headers();
-    myHeaders.append("apikey", "oO66cl65R143rZd5CDXtAsTfTL9BVvXi");
-
-    fetch("https://api.apilayer.com/exchangerates_data/symbols", {
-      method: 'GET',
-      redirect: 'follow',
-      headers: myHeaders
-    })
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-  }
 
 }
